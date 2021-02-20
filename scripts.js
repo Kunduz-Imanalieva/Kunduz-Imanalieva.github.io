92d6a6fabce26750f40719c7dcf36c030ce83a6d
Kunduz-Imanalieva <imakuzya@gmail.com>
2021-02-20 21:17:06 +0600

    selected all three inputs and a button in  js

---
+++ scripts.js
@@ -0,0 +1,10 @@
+let button = document.querySelector("button");
+let nameInput = document.querySelector("#name");
+let messageInput = document.querySelector("#message");
+let colorInput = document.querySelector("#color");
+button.addEventListener("mousedown", function () {
+  //   console.log(nameInput.value);
+  //   console.log(messageInput.value);
+  //   console.log(colorInput.value);
+
+});
